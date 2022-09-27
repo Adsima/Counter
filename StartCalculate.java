83ff23959cca20d058c4e4166651c88c1845df66
Arsen Minnegulov <sahalysyk02@mail.ru>
2022-09-27 18:35:55 +0500

    Next release

--- StartCalculate.java
+++ StartCalculate.java
@@ -1,5 +1,8 @@
 package calculate;
 
+import calculate.service.TaskService;
+import calculate.strategy.Strategy;
+
 import java.io.IOException;
 
 import static calculate.util.ConsoleUtil.print;
@@ -7,18 +10,21 @@ import static calculate.util.MessageConstants.GREETINGS_STRING;
 
 public class StartCalculate {
 
-//    TaskService taskService;
-//    StartCalculate() throws IOException {
-//        taskService.runTask();
-//    }
+    TaskService taskService;
+    Strategy strategy;
+    StartCalculate() {
+        taskService = new TaskService();
+    }
 
     public static void main(String[] args) throws IOException {
-        new StartCalculate();
+        new StartCalculate()
+                .start();
     }
 
     private void start() {
         print(GREETINGS_STRING);
 
+        taskService.runTask(strategy.strategy());
 
     }
 }
---
+++ strategy/Strategy.java
@@ -0,0 +1,7 @@
+package calculate.strategy;
+
+import calculate.model.task.Task;
+
+public interface Strategy {
+    Task strategy();
+}
